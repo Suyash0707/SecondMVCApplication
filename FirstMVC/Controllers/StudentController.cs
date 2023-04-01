370a2a5fa7b61862f1128e06fa3c860684783f73
Karpe Aditya Mohan <Karpe Aditya Mohan@LAPTOP-GRP69F4I>
2023-04-01 23:35:45 +0530

    Add project files.

---
+++ FirstMVC/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondDOTNETApp.Models;
+
+namespace SecondDOTNETApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index() 
+        {
+            SecondMvcappContext db = new SecondMvcappContext();
+            var students = db.Students.ToList();
+
+            return View(students);
+        }
+    }
+}
---
+++ FirstMVC/Models/SecondMvcappContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecondDOTNETApp.Models;
+
+public partial class SecondMvcappContext : DbContext
+{
+    public SecondMvcappContext()
+    {
+    }
+
+    public SecondMvcappContext(DbContextOptions<SecondMvcappContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("server=localhost; database=SecondMVCApp; Trusted_Connection=true; TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC079D067C8C");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
